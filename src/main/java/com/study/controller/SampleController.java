8c6c7edf392919b3df767c53034c739c41fc24c4
w-beom <60081555+w-beom@users.noreply.github.com>
2020-07-13 23:51:22 +0900

    130~143 페이지
    ### 공부한 거
    - RequestParam List,Array,객체로 파라미터 보내기
    - @ModelAttribute 사용
    - @initBinder
    - @DateTimeFormat

--- src/main/java/com/study/controller/SampleController.java
+++ src/main/java/com/study/controller/SampleController.java
@@ -1,10 +1,22 @@
 package com.study.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.study.domain.SampleDTO;
+import com.study.domain.SampleDTOList;
+import com.study.domain.TodoDTO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -12,12 +24,64 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/sample/*")
 @Log4j
 public class SampleController {
-	
+
 	@GetMapping("/ex01")
 	public String basic(SampleDTO dto) {
-		log.info(""+ dto);
-		
+		log.info("" + dto);
+		System.out.println("adsadsadadasdadsdasdads");
+
 		return "ex01";
+	}
+
+	@GetMapping("/ex02")
+	public String ex02(@RequestParam("name") String name, @RequestParam("age") int age) {
+		log.info("name : " + name);
+		log.info("age : " + age);
+
+		return "ex02";
+	}
+
+	@GetMapping("/ex02List")
+	public String ex02List(@RequestParam("ids") ArrayList<String> ids) {
+		log.info("ids : " + ids);
+
+		return "ex02List";
+	}
+	
+	@GetMapping("/ex02Array")
+	public String ex02Array(@RequestParam("ids") String[] ids) {
+		log.info("arrays ids : " + Arrays.toString(ids));
+
+		return "ex02Array";
+	}
+	
+	@GetMapping("/ex02Bean")
+	public String ex02Bean(SampleDTOList list) {
+		log.info("list dtos : "+list);
+		
+		return "ex02Bean";
+	}
+	
+	@GetMapping
+	public String ex03(TodoDTO todo) {
+		log.info("todo : "+todo);
+		return "ex03";
+	}
+	
+	@GetMapping("/home")
+	public String home(Model model) {
+		model.addAttribute("servierTime", new java.util.Date());
+		
+		return "home";
+	}
+	
+	@GetMapping("/ex04")
+	public String ex04(SampleDTO dto, @ModelAttribute("page") int page) {
+		log.info("dto + "+dto);
+		log.info("page : "+page);
+		
+		return "/sample/ex04";
+		
 	}
 
 }
